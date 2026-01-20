73900a3abc7bfc8749770c50f3e5c361f6507f65
thefool309 <themagician309@gmail.com>
2026-01-19 22:08:51 -0600

    put finishing touches on build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # this script is just going to take whatever asm file we feed it and compile it as elf64
 # usage build.sh $FILENAME (include extension)
 FILENAME=$1
-NAME="$FILENAME%.*"
+NAME="${FILENAME%.*}"
 
 shutdown() {
   echo "something went wrong"
@@ -38,20 +38,44 @@ fi
 
 echo "made it past the second test"
 
+echo "$FILENAME"
+echo "$NAME"
+
+continueOrNah
+
+
 build() {
+
+  set -x
+  
   readonly SCRIPT_DIR="$(cd -- "$(dirname -- "$BASH_SOURCE")" && pwd)"
 
   echo "$SCRIPT_DIR"
   
+  SRC_DIR="${SCRIPT_DIR}/src"
+
+  echo "$SRC_DIR"
+  
+  BIN_DIR="$SCRIPT_DIR/bin"
+  
+  echo "$BIN_DIR"
+  
   continueOrNah
 
+  FILEPATH="${SRC_DIR}/${FILENAME}"
 
-  BIN_DIR="$SCRIPT_DIR/bin"
+  OBJECTPATH="${BIN_DIR}/${NAME}.o"
+  
   if [[ ! -d BIN_DIR ]]; then
     mkdir -p "$BIN_DIR"
   fi
-  nasm -f elf64 -o "${BIN_DIR}/${NAME}.o" "${SCRIPT_DIR}/${FILENAME}"
-
+  if [[ ! -f "$FILEPATH" ]]; then
+    echo "File at: '${FILEPATH}' does not exist"
+    shutdown
+  fi
+  nasm -f elf64 -o "${OBJECTPATH}" "${FILEPATH}"
+  ld -o "${BIN_DIR}/${NAME}" ${OBJECTPATH}
+  set +x
 }
 
 build
