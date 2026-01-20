a1b33ee5f4a88f7930d7f9a03b7a30d49cfed689
thefool309 <themagician309@gmail.com>
2026-01-19 22:26:52 -0600

    started basic guessing game in  to get in the groove of things

--- src/hello.asm
+++ src/hello.asm
@@ -2,7 +2,12 @@ BITS 64
 
 section .data
     text db "Hello, World!",10
- 
+    prompt db "Enter a number between 1-10: ",0
+    promptLen equ $ - prompt
+
+section .bss
+    buffer resb 16
+
 section .text
     global _start
  
@@ -13,6 +18,25 @@ _start:
     mov rdx, 14
     syscall
  
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, prompt
+    mov rdx, promptLen
+    syscall
+
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, buffer
+    mov rdx, 16
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 16
+    syscall
+
     mov rax, 60
     mov rdi, 0
     syscall
+
+
