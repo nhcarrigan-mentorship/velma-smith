08cdf0619872792b515f82de2103a610a5bd758b
thefool309 <themagician309@gmail.com>
2026-01-24 20:51:39 -0600

    finished up core guessing game.

--- src/hello.asm
+++ src/hello.asm
@@ -1,8 +1,8 @@
 BITS 64
 
-%ifdef LINUX
-  %define SYSCALL_EXIT 60
-%elifdef FREEBSD
+
+%define SYSCALL_EXIT 60
+%ifdef FREEBSD
   %define SYSCALL_EXIT 1
 %endif
 
@@ -13,8 +13,10 @@ section .data
     promptLen equ $ - prompt
     fun_text db "The best function has been called.", 10
     fun_textLen equ $ - fun_text
-    fail_text db "Wrong. Try Again!", 10
-    fail_textLen equ $ - fail_text
+    lower_text db "Wrong. Guess is less than Secret. Try Again!", 10
+    lower_textLen equ $ - lower_text
+    higher_text db "Wrong Guess is greater than Secret. Try Again!", 10
+    higher_textLen equ $ - higher_text
     success_text db "You succeeded! :3"
     success_textLen equ $ - success_text
     continue_text db "Continue?", 10
@@ -32,6 +34,9 @@ _start:
     mov rsi, text
     mov rdx, 14
     syscall
+
+    call random
+
 .prompt: 
     mov rax, 1
     mov rdi, 1
@@ -39,15 +44,17 @@ _start:
     mov rdx, promptLen
     syscall
 
+    call zero_out_buffer
+
     mov rax, 0
     mov rdi, 0
     mov rsi, buffer
-    mov rdx, 16
+    mov rdx, 15
     syscall
 
     call atoi
 
-    mov r10, rax
+    mov r12, rax
 
     mov rdx, rax
     mov rax, 1
@@ -55,28 +62,36 @@ _start:
     mov rsi, buffer
     syscall
 
-    call random
     
-    cmp r10, r11
+    cmp r12, r13
     
-    jne failure
-
-    jmp _exit
+    jl handle_lower
+    jg handle_higher
+    
+    je success
     
 _exit:
     mov rax, SYSCALL_EXIT
     mov rdi, 0
     syscall
-
-failure:
+handle_lower:
     xor rsi, rsi
 
     mov rax, 1
     mov rdi, 1
-    mov rsi, fail_text
-    mov rdx, fail_textLen
+    mov rsi, lower_text
+    mov rdx, lower_textLen
     syscall
     jmp _start.prompt 
+handle_higher:
+    xor rsi, rsi
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, higher_text
+    mov rdx, higher_textLen
+    syscall
+    jmp _start.prompt
 
 random:
     rdrand rax
@@ -86,7 +101,7 @@ random:
     mov rcx, 10
     div rcx
     inc rdx
-    mov r11, rdx
+    mov r13, rdx
 
     ret
 
@@ -123,11 +138,7 @@ continue:
     mov rdx, continue_textLen
     syscall
     
-    lea rdi, [buffer]
-    mov rcx, 2
-    xor eax, eax
-    cld
-    rep stosq
+    call zero_out_buffer
 
     mov rax, 0
     mov rdi, 0
@@ -136,6 +147,21 @@ continue:
     syscall
 
     mov byte [rsi + rax], 0
+
+zero_out_buffer:
+    lea rdi, [buffer]
+    mov rcx, 2
+    xor eax, eax
+    cld
+    rep stosq
+    ret
+
+
+success:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, success_text
+    mov rdx, success_textLen
     syscall
 
-
+    jmp _exit
