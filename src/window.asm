63bb7fff5ecf17d1bed4f0187720a141eedd4e9f
thefool309 <themagician309@gmail.com>
2026-01-25 14:05:23 -0600

    added function to create and connect with unix socket

--- src/window.asm
+++ src/window.asm
@@ -20,7 +20,8 @@ sun_path: db "/tmp/.X11-unix/X0", 0
 static sun_path:data
 
 section .text
-
+; Create a UNIX domain socket and connect to the X11 server.
+; @returns The Socket file descriptor
 x11_connect_to_server:
 static x11_connect_to_server:function
   push rbp
@@ -33,29 +34,46 @@ static x11_connect_to_server:function
   mov rdx, 0
   syscall
   
-  cmp rax, 0
+  cmp rax, 0  ; check return value of previous syscall
   jle die
 
   mov rdi, rax  ; store socket fd in `rdi`
 
-  sub rsp, 112
+  sub rsp, 112  ; make room for sock_addr struct 
 
-  mov WORD [rsp], AF_UNIX
+  mov WORD [rsp], AF_UNIX ; set socket family to AF_UNIX
 
+  ; fill structs sun_path with: "/tmp/.X11-unix/X0"
   lea rsi, sun_path
-  mov r12, rdi
+  mov r12, rdi ; store socket fd in `r12`
   lea rdi, [rsp + 2]
   cld
   mov ecx, 19
   rep movsb
   
+  ; Connect to the server: connect(2).
+  mov rax, SYSCALL_CONNECT
+  mov rdi, r12
+  lea rsi, [rsp]
+  %define SIZEOF_SOCKADDR_UN 2+108
+  mov rdx, SIZEOF_SOCKADDR_UN
+  syscall
+
+  cmp rax, 0 ; check return value of previous syscall 
+  jne die
+
+  mov rax, rdi ; Return the socket file descriptor
+
+  add rsp, 112 
   pop rbp
+  
   ret
 
 exit:
   mov rax, SYSCALL_EXIT
   xor edi, edi
   syscall
+
 die:
   mov rax, SYSCALL_EXIT
   mov rdi, EXIT_ERROR
@@ -63,5 +81,5 @@ die:
 
 _start:
 global _start:function
-
+  call x11_connect_to_server
   jmp exit
